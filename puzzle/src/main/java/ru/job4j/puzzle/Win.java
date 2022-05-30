c284766e6b3bb45654cde92f46cdc124cf3f3b46
RustamSaydam <rustamsaydam@icloud.com>
2022-05-30 21:44:22 +0300

    2. Сокобан. Тестовое задание.

--- puzzle/src/main/java/ru/job4j/puzzle/Win.java
+++ puzzle/src/main/java/ru/job4j/puzzle/Win.java
@@ -1,8 +1,44 @@
 package ru.job4j.puzzle;
 
 public class Win {
+
     public static boolean check(int[][] board) {
-        boolean rsl = true;
+        boolean rsl = false;
+        for (int i = 0; i < board.length; i++) {
+            if (board[i][i] == 1) {
+                if (Win.checkWinColumn(board, i) || Win.checkWinRow(board, i)) {
+                    rsl = true;
+                }
+            }
+        }
+
         return rsl;
     }
+
+    public static boolean checkWinRow(int[][] board, int row) {
+        boolean rsl = true;
+        for(int column = 0; column < board.length; column++) {
+            if (board [row][column] == 0) {
+                rsl = false;
+                break;
+            }
+        }
+        return rsl;
+    }
+
+    public static boolean checkWinColumn(int[][] board, int column) {
+      boolean rsl = true;
+      for(int row = 0; row < board.length; row++) {
+          if(board[row][column] == 0) {
+              rsl = false;
+              break;
+          }
+      }
+      return rsl;
+    }
 }
+
+
+
+
+
