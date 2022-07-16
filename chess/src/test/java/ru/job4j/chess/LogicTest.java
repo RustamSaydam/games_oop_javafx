84c3ec28aa2d3ac8a48ef0605476b8013c8c57e4
RustamSaydam <rustamsaydam@icloud.com>
2022-07-16 19:17:29 +0300

    2. Каркас шахматной доски Доп файл

--- chess/src/test/java/ru/job4j/chess/LogicTest.java
+++ chess/src/test/java/ru/job4j/chess/LogicTest.java
@@ -6,7 +6,6 @@ import ru.job4j.chess.firuges.Cell;
 import ru.job4j.chess.firuges.black.BishopBlack;
 import ru.job4j.chess.firuges.black.RookBlack;
 
-
 public class LogicTest {
 
     @Test(expected = OccupiedCellException.class)
